53ef27def0eb26f460ce634d2939bf04ec09127d
BrianM0330 <BrianM0330@gmail.com>
2026-05-13 17:36:13 -0500

    feat(db): cleanup deletes

--- db/migrate/20260513201101_create_likes.rb
+++ db/migrate/20260513201101_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[8.1]
   def change
     create_table :likes do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :photo, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }
+      t.references :photo, null: false, foreign_key: { on_delete: :cascade }
 
       t.timestamps
     end
--- db/migrate/20260513201102_create_comments.rb
+++ db/migrate/20260513201102_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[8.1]
   def change
     create_table :comments do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :photo, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: { on_delete: :cascade }
+      t.references :photo, null: false, foreign_key: { on_delete: :cascade }
       t.text :body, null: false
 
       t.timestamps
