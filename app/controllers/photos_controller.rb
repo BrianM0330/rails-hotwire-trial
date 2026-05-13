e9455090e0c6c94feabb504c3ddff6d935283cc2
BrianM0330 <BrianM0330@gmail.com>
2026-05-13 18:14:20 -0500

    fix(nav): redirect auth/unauth users to landing/gallery

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,11 @@
 class PhotosController < ApplicationController
+  before_action :redirect_unauthenticated_to_root
+
   def index
   end
+
+  private
+    def redirect_unauthenticated_to_root
+      redirect_to root_path unless authenticated?
+    end
 end
